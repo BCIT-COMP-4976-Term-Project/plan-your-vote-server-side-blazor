a83726ef0e6a2235b8349b89bb3cefcb2d267ca6
nickybittencourt <nickytuon75@gmail.com>
2020-11-10 03:27:19 -0300

    Generated template for a blazor server app

---
+++ Data/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace plan_your_vote_server_side_blazor.Data
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
